03415e2873e995ba42c7b6dffc30bb7279585894
FeliciaSithole <feliciasithole.fs@gmail.com>
2025-09-19 00:12:46 +0200

    created table for products

--- answers.sql
+++ answers.sql
@@ -9,3 +9,12 @@ CREATE TABLE Customers (
     phone VARCHAR(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE Products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
